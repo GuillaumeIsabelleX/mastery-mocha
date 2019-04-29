f92764682464cad1708c86105501f68d37f20f62
Guillaume Isabelle <jgi@jgwill.com>
2019-04-29 13:52:52 -0400

    dotenv setup

--- test/converter.js
+++ test/converter.js
@@ -2,14 +2,15 @@ var expect    = require("chai").expect;
 var converter = require("../app/converter");
 
 
-describe("Color Code Converter", function() {
-  describe("RGB to Hex conversion", function() {
-    it("converts the basic colors", function() {
+describe("Dummy Test", function() {
+  describe("Just dummy expectations", function() {
+    it("Just check a variable", function() {
       var x = 0;
       var y = 0;
-      var z = x / y; //@test Expecting to crash the test
+      
       expect(x,"Not good value").to.equal(0);
     });
+    
   });
 
   
