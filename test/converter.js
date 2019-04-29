11106a8a3d86592df520c1bbb84d6a2a53c3684e
Guillaume Isabelle <jgi@jgwill.com>
2019-04-29 13:55:17 -0400

    test: added an env test

--- test/converter.js
+++ test/converter.js
@@ -1,17 +1,24 @@
-var expect    = require("chai").expect;
+var expect = require("chai").expect;
 var converter = require("../app/converter");
 
 
-describe("Dummy Test", function() {
-  describe("Just dummy expectations", function() {
-    it("Just check a variable", function() {
+describe("Dummy Test", function () {
+  describe("Just dummy expectations", function () {
+    it("Just check a variable", function () {
       var x = 0;
       var y = 0;
-      
-      expect(x,"Not good value").to.equal(0);
+
+      expect(x, "Not good value").to.equal(0);
     });
-    
+
+    it('Read an ENV Var and check its value', () => {
+
+      require('dotenv').config();
+      var tstVar = process.env.MYVAR;
+
+      expect(tstVar,"Invalid environment variable").to.equal(2);
+    });
   });
 
-  
+
 });
